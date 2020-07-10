aace80f815f8edcd7696f43c60f6e3772f18644b
Ha Min-ho <ha_mino@naver.com>
2020-07-11 01:41:16 +0900

    volumeCredits 함수 인라인

--- createStatementData.js
+++ createStatementData.js
@@ -51,17 +51,13 @@ function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
     result.play = calculator.play;
     result.amount = calculator.amount;
-    result.volumeCredits = volumeCreditsFor(result);
+    result.volumeCredits = calculator.volumeCredits;
     
     return result;
 }
 
 function playFor(aPerformance) {
     return plays[aPerformance.playID];
-}
-
-function volumeCreditsFor(aPerformance) {
-    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).volumeCredits;
 }
 
 function totalVolumeCredits(data) {
