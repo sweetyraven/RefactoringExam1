5d4baea83aee11017a243547d7c7e767b387cca1
Ha Min-ho <ha_mino@naver.com>
2020-07-11 01:30:17 +0900

    PerformanceCalculator 클래스 생성

--- createStatementData.js
+++ createStatementData.js
@@ -1,4 +1,10 @@
 
+class PerformanceCalculator{
+    constructor(aPerformance) {
+        this.performance = aPerformance;
+    }
+}
+
 function createStatementData(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
@@ -9,6 +15,7 @@ function createStatementData(invoice, plays) {
 
     
 function enrichPerformance(aPerformance) {
+    const calculator = new PerformanceCalculator(aPerformance);
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
     result.amount = amountFor(result);
--- state.js
+++ state.js
@@ -22,3 +22,18 @@ function renderPlainText(data) {
     }
 
 }
+
+function htmlStatement(invoice, plays) {
+    return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+    let result = '<h1>청구 내역 (고객명: ${data.customer})</h1>\n';
+    result += "<table>\n";
+    result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
+    for(let perf of data.performances) {
+        result += ' <tr>'
+    }
+
+    return result;
+}
