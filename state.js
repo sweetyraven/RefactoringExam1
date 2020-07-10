d0a50c809db0f7732b8d0b4efac24624b85fa596
Ha Min-ho <ha_mino@naver.com>
2020-07-11 00:46:06 +0900

    totalAmount 초기화 문장 슬라이드

--- state.js
+++ state.js
@@ -1,5 +1,4 @@
 function statement(invoice, plays) {
-    let totalAmount = 0;
     let result = '청구 내역 (고객명: ${invoice.customer})\n';
 
     for(let perf of invoice.performances) {
@@ -7,6 +6,7 @@ function statement(invoice, plays) {
         result += '   ${playFor(perf).name}: ${usd(thisAmmount/100)} ($perf.audience}석)\n';
     }
 
+    let totalAmount = 0;
     for(let perf of invoice.performances) {
         totalAmount += amountFor(perf);
     }
