aac917a2fa1dfd66fab54ca7383fc0f9253aed48
Ha Min-ho <ha_mino@naver.com>
2020-07-11 00:35:42 +0900

    volumeCreditsFor 내의 volumeCredits 변수 이름 변경

--- state.js
+++ state.js
@@ -17,10 +17,11 @@ function statement(invoice, plays) {
     }
 
     function volumeCreditsFor(perf) {
-        let volumeCredits = 0;
-        volumeCredits += Math.max(perf.audience - 30, 0);
+        let result = 0;
+        result += Math.max(perf.audience - 30, 0);
         if ("comedy" == playFor(perf).type)
-            volumeCredits += Math.floor(perf.audience / 5);
+            result += Math.floor(perf.audience / 5);
+        return result;
     }
 
     function amountFor(aPerformance) {
