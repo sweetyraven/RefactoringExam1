371da19b5bd66393d4073c739c20567878bbd5e5
Ha Min-ho <ha_mino@naver.com>
2020-07-11 00:59:57 +0900

    invoice.performances 데이터 옮김

--- state.js
+++ state.js
@@ -1,6 +1,7 @@
 function statement(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances;
     return renderPlainText(statementData, invoice, plays);
 }
 
@@ -8,7 +9,7 @@ function statement(invoice, plays) {
 function renderPlainText(data, invoice, plays) {
     let result = '청구 내역 (고객명: ${data.customer})\n';
 
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
         // 청구 내역을 출력한다.
         result += '   ${playFor(perf).name}: ${usd(thisAmmount/100)} ($perf.audience}석)\n';
     }
@@ -19,7 +20,7 @@ function renderPlainText(data, invoice, plays) {
 
     function totalAmount() {
         let result = 0;
-        for (let perf of invoice.performances) {
+        for (let perf of data.performances) {
             result += amountFor(perf);
         }
 
@@ -28,7 +29,7 @@ function renderPlainText(data, invoice, plays) {
 
     function totalVolumeCredits() {
         let volumeCredits = 0;
-        for (let perf of invoice.performances) {
+        for (let perf of data.performances) {
             volumeCredits += volumeCreditsFor(perf);
         }
 
