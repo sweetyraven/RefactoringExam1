df4ce29565fae7bc9399b49a58083eca1d399292
Ha Min-ho <ha_mino@naver.com>
2020-11-08 14:02:54 +0900

    테스트 추가

---
+++ Test/test.js
@@ -0,0 +1,12 @@
+
+const statement = require("../state");
+
+const invoice = require('../invoice.json');
+const plays = require('../plays.json');
+
+test("출력 검사", () => {
+
+    const expected = '청구 내역 (고객명: BigCo)\n   Hamlet: $650.00 (55석)\n총액: $650.00\n적립 포인트: 25점\n';
+    expect('abc').toEqual('abc');
+    expect(expected).toEqual(statement(invoice, plays));
+})
--- state.js
+++ state.js
@@ -1,7 +1,7 @@
 function statement(invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
-    let result = '청구 내역 (고객명: ${invoice.customer})\n';
+    let result = `청구 내역 (고객명: ${invoice.customer})\n`;
     const format = new Intl.NumberFormat("en-US",
     { style: "currency", currency: "USD", minimumFractionDigits: 2}).format;
 
@@ -10,7 +10,7 @@ function statement(invoice, plays) {
         let thisAmount = 0;
 
         switch (play.type) {
-            case "tregedy": // 비극
+            case "tragedy": // 비극
               thisAmount = 40000;
               if (perf.audience > 30) {
                   thisAmount += 1000 * (perf.audience - 30);
@@ -24,7 +24,7 @@ function statement(invoice, plays) {
               thisAmount += 300 * perf.audience;
               break;
             default:
-                throw new Error('알 수 없는 장르: ${play.type}');
+                throw new Error(`알 수 없는 장르: ${play.type}`);
         }
 
         // 포인트를 적립한다.
@@ -33,10 +33,12 @@ function statement(invoice, plays) {
         if("comedy" == play.type) volumeCredits += Math.floor(perf.audience / 5);
 
         // 청구 내역을 출력한다.
-        result += '   ${play.name}: ${format(thisAmmount/100)} ($perf.audience}석)\n';
+        result += `   ${play.name}: ${format(thisAmount/100)} (${perf.audience}석)\n`;
         totalAmount += thisAmount;
-        result += '총액: ${format(totalAmount/100)}\n';
-        result += '적립 포인트: ${volumeCredits}점\n';
+        result += `총액: ${format(totalAmount/100)}\n`;
+        result += `적립 포인트: ${volumeCredits}점\n`;
         return result;
     }
 }
+
+module.exports = statement;
