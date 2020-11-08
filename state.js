51245b3498c282f3cb4054c1d25d107839cee41a
Ha Min-ho <ha_mino@naver.com>
2020-11-08 23:52:08 +0900

    amountFor 함수를 내부함수로 옮김

--- state.js
+++ state.js
@@ -21,29 +21,30 @@ function statement(invoice, plays) {
         result += `적립 포인트: ${volumeCredits}점\n`;
         return result;
     }
-}
 
-function amountFor(aPerformance, play) { 
+    function amountFor(aPerformance, play) { 
     let result = 0;
     switch (play.type) {
         case "tragedy": // 비극
-          result = 40000;
-          if (aPerformance.audience > 30) {
-              result += 1000 * (aPerformance.audience - 30);
-          }
-          break;
+        result = 40000;
+        if (aPerformance.audience > 30) {
+            result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
         case "comedy": //희극
-          result = 30000;
-          if (aPerformance.audience > 20) {
-              result += 10000 + 500 * (aPerformance.audience - 20);
-          }
-          result += 300 * aPerformance.audience;
-          break;
+        result = 30000;
+        if (aPerformance.audience > 20) {
+            result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
         default:
             throw new Error(`알 수 없는 장르: ${play.type}`);
     }
 
     return result;
+}
+
 }
 
 module.exports = statement;
