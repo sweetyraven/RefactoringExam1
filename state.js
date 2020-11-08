d8a89a0a6ace38857f921da777163c8a17b23678
Ha Min-ho <ha_mino@naver.com>
2020-11-08 23:53:17 +0900

    인덴트 수정

--- state.js
+++ state.js
@@ -23,28 +23,27 @@ function statement(invoice, plays) {
     }
 
     function amountFor(aPerformance, play) { 
-    let result = 0;
-    switch (play.type) {
-        case "tragedy": // 비극
-        result = 40000;
-        if (aPerformance.audience > 30) {
-            result += 1000 * (aPerformance.audience - 30);
+        let result = 0;
+        switch (play.type) {
+            case "tragedy": // 비극
+            result = 40000;
+            if (aPerformance.audience > 30) {
+                result += 1000 * (aPerformance.audience - 30);
+            }
+            break;
+            case "comedy": //희극
+            result = 30000;
+            if (aPerformance.audience > 20) {
+                result += 10000 + 500 * (aPerformance.audience - 20);
+            }
+            result += 300 * aPerformance.audience;
+            break;
+            default:
+                throw new Error(`알 수 없는 장르: ${play.type}`);
         }
-        break;
-        case "comedy": //희극
-        result = 30000;
-        if (aPerformance.audience > 20) {
-            result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-        default:
-            throw new Error(`알 수 없는 장르: ${play.type}`);
+
+        return result;
     }
-
-    return result;
-}
-
 }
 
 module.exports = statement;
